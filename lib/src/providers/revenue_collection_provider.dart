fb03a84a4d4a9a1817081ce4c335518b46b240ee
Chris <chrismelky05@gmail.com>
2023-09-06 12:06:40 +0300

    add try catch

--- lib/src/providers/revenue_collection_provider.dart
+++ lib/src/providers/revenue_collection_provider.dart
@@ -15,7 +15,6 @@ import 'package:zanmutm_pos_client/src/models/receipt.dart';
 import 'package:zanmutm_pos_client/src/models/revenue_source.dart';
 import 'package:zanmutm_pos_client/src/models/user.dart';
 import 'package:zanmutm_pos_client/src/providers/pos_status_provider.dart';
-import 'package:zanmutm_pos_client/src/providers/revenue_source_provider.dart';
 import 'package:zanmutm_pos_client/src/services/pos_transaction_service.dart';
 import 'package:zanmutm_pos_client/src/services/service.dart';
 import 'package:zanmutm_pos_client/src/utils/helpers.dart';
@@ -179,117 +178,129 @@ class RevenueCollectionProvider extends ChangeNotifier
   }
 
   Future<String?> printSunMi(Receipt r, List<RevenueItem> items) async {
-    bool? connected = await SunmiPrinter.bindingPrinter();
-    if (connected == true) {
-      await SunmiPrinter.startTransactionPrint(true);
-      if (r.logo != null) {
-        await SunmiPrinter.setAlignment(SunmiPrintAlign.CENTER);
-        await SunmiPrinter.printImage(r.logo!);
-      }
-      await SunmiPrinter.lineWrap(2);
-      await SunmiPrinter.printText(r.gov,
-          style: SunmiStyle(bold: true, align: SunmiPrintAlign.CENTER));
-      await SunmiPrinter.printText(r.council,
-          style: SunmiStyle(bold: true, align: SunmiPrintAlign.CENTER));
-      await SunmiPrinter.line();
-      await SunmiPrinter.printText(r.phone,
-          style: SunmiStyle(
-              align: SunmiPrintAlign.CENTER, fontSize: SunmiFontSize.SM));
-      await SunmiPrinter.printText(r.email,
-          style: SunmiStyle(
-              align: SunmiPrintAlign.CENTER, fontSize: SunmiFontSize.SM));
-      await SunmiPrinter.line();
-      await SunmiPrinter.printText(r.title, style: SunmiStyle(bold: true));
-      await SunmiPrinter.setAlignment(SunmiPrintAlign.LEFT);
-      await SunmiPrinter.printText(r.recNumber,
-          style: SunmiStyle(fontSize: SunmiFontSize.MD));
-      await SunmiPrinter.printText(r.payer,
-          style: SunmiStyle(fontSize: SunmiFontSize.MD));
-      await SunmiPrinter.printText(r.receTotal,
-          style: SunmiStyle(fontSize: SunmiFontSize.MD));
-      await SunmiPrinter.printText(r.status,
-          style: SunmiStyle(fontSize: SunmiFontSize.MD));
-      await SunmiPrinter.lineWrap(1); // Jump 2 lines
-      // Center align
-      for (var item in items) {
-        await SunmiPrinter.printText(
-            '${item.revenueSource.name}   ${item.quantity} x ${currency.format(item.amount)}',
+    try {
+      bool? connected = await SunmiPrinter.bindingPrinter();
+      if (connected == true) {
+        await SunmiPrinter.startTransactionPrint(true);
+        if (r.logo != null) {
+          await SunmiPrinter.setAlignment(SunmiPrintAlign.CENTER);
+          await SunmiPrinter.printImage(r.logo!);
+        }
+        await SunmiPrinter.lineWrap(2);
+        await SunmiPrinter.printText(r.gov,
+            style: SunmiStyle(bold: true, align: SunmiPrintAlign.CENTER));
+        await SunmiPrinter.printText(r.council,
+            style: SunmiStyle(bold: true, align: SunmiPrintAlign.CENTER));
+        await SunmiPrinter.line();
+        await SunmiPrinter.printText(r.phone,
             style: SunmiStyle(
-                align: SunmiPrintAlign.RIGHT, fontSize: SunmiFontSize.MD));
+                align: SunmiPrintAlign.CENTER, fontSize: SunmiFontSize.SM));
+        await SunmiPrinter.printText(r.email,
+            style: SunmiStyle(
+                align: SunmiPrintAlign.CENTER, fontSize: SunmiFontSize.SM));
+        await SunmiPrinter.line();
+        await SunmiPrinter.printText(r.title, style: SunmiStyle(bold: true));
+        await SunmiPrinter.setAlignment(SunmiPrintAlign.LEFT);
+        await SunmiPrinter.printText(r.recNumber,
+            style: SunmiStyle(fontSize: SunmiFontSize.MD));
+        await SunmiPrinter.printText(r.payer,
+            style: SunmiStyle(fontSize: SunmiFontSize.MD));
+        await SunmiPrinter.printText(r.receTotal,
+            style: SunmiStyle(fontSize: SunmiFontSize.MD));
+        await SunmiPrinter.printText(r.status,
+            style: SunmiStyle(fontSize: SunmiFontSize.MD));
+        await SunmiPrinter.lineWrap(1); // Jump 2 lines
+        // Center align
+        for (var item in items) {
+          await SunmiPrinter.printText(
+              '${item.revenueSource.name}   ${item.quantity} x ${currency.format(item.amount)}',
+              style: SunmiStyle(
+                  align: SunmiPrintAlign.RIGHT, fontSize: SunmiFontSize.MD));
+        }
+        await SunmiPrinter.line();
+        await SunmiPrinter.printText(r.paid,
+            style: SunmiStyle(bold: true, align: SunmiPrintAlign.RIGHT));
+        await SunmiPrinter.lineWrap(2);
+        await SunmiPrinter.printText(r.paidDate,
+            style: SunmiStyle(fontSize: SunmiFontSize.MD));
+        await SunmiPrinter.printText(r.printedBy,
+            style: SunmiStyle(fontSize: SunmiFontSize.MD));
+        await SunmiPrinter.lineWrap(1);
+        await SunmiPrinter.setAlignment(SunmiPrintAlign.CENTER);
+        await SunmiPrinter.printQRCode(r.qr, size: 3);
+        await SunmiPrinter.lineWrap(4);
+        await SunmiPrinter.submitTransactionPrint(); // SUBMIT and cut paper
+        await SunmiPrinter.exitTransactionPrint(true);
+        await SunmiPrinter.unbindingPrinter();
+        return null;
       }
-      await SunmiPrinter.line();
-      await SunmiPrinter.printText(r.paid,
-          style: SunmiStyle(bold: true, align: SunmiPrintAlign.RIGHT));
-      await SunmiPrinter.lineWrap(2);
-      await SunmiPrinter.printText(r.paidDate,
-          style: SunmiStyle(fontSize: SunmiFontSize.MD));
-      await SunmiPrinter.printText(r.printedBy,
-          style: SunmiStyle(fontSize: SunmiFontSize.MD));
-      await SunmiPrinter.lineWrap(1);
-      await SunmiPrinter.setAlignment(SunmiPrintAlign.CENTER);
-      await SunmiPrinter.printQRCode(r.qr, size: 3);
-      await SunmiPrinter.lineWrap(4);
-      await SunmiPrinter.submitTransactionPrint(); // SUBMIT and cut paper
-      await SunmiPrinter.exitTransactionPrint(true);
-      await SunmiPrinter.unbindingPrinter();
-      return null;
+      debugPrint("not connected");
+      return 'Print not connected';
+    } catch(e) {
+      notifyError(e.toString());
+      return e.toString();
     }
-    debugPrint("not connected");
-    return 'Print not connected';
+
   }
 
   Future<String?> printMobiIot(Receipt r, List<RevenueItem> items) async {
-    String line = "-----------------------------------------";
-    bool? connected = await MobiiotPrinter.bindingPrinter();
-    if (connected == true) {
-      if (r.logo != null) {
-        await MobiiotPrinter.setAlignment(1);
-        await MobiiotPrinter.printImage(r.logo!);
+    try {
+      String line = "-----------------------------------------";
+      bool? connected = await MobiiotPrinter.bindingPrinter();
+      if (connected == true) {
+        if (r.logo != null) {
+          await MobiiotPrinter.setAlignment(1);
+          await MobiiotPrinter.printImage(r.logo!);
+        }
+        await MobiiotPrinter.lineWrap(2);
+        await MobiiotPrinter.printText(r.gov, style: {"bold": true, "align": 1});
+        await MobiiotPrinter.printText(r.council,
+            style: {"bold": true, "align": 1});
+        await MobiiotPrinter.printText(line, style: {"align": 1, "font": 1});
+        await MobiiotPrinter.printText(r.phone, style: {"font": 1, "align": 1});
+        await MobiiotPrinter.printText(r.email, style: {"font": 1, "align": 1});
+        await MobiiotPrinter.printText(line, style: {"align": 1, "font": 1});
+        await MobiiotPrinter.printText(r.title,
+            style: {"bold": true, "align": 1});
+        await MobiiotPrinter.printText(r.recNumber, style: {
+          "font": 1,
+          "bold": false,
+        });
+        await MobiiotPrinter.printText(r.payer, style: {
+          "font": 1,
+          "bold": false,
+        });
+        await MobiiotPrinter.printText(r.receTotal, style: {
+          "font": 1,
+          "bold": false,
+        });
+        await MobiiotPrinter.printText(r.status, style: {
+          "font": 1,
+          "bold": false,
+        });
+        await MobiiotPrinter.lineWrap(1); // Jump 2 lines
+        // Center align
+        for (var item in items) {
+          await MobiiotPrinter.printText(
+              '${item.revenueSource.name}   ${item.quantity} x ${currency.format(item.amount)}',
+              style: {"font": 1, "align": 2});
+        }
+        await MobiiotPrinter.printText(line, style: {"align": 1, "font": 1});
+        await MobiiotPrinter.printText(r.paid, style: {"align": 2, "font": 1});
+        await MobiiotPrinter.lineWrap(2);
+        await MobiiotPrinter.printText(r.paidDate, style: {"font": 1});
+        await MobiiotPrinter.printText(r.printedBy, style: {"font": 1});
+        await MobiiotPrinter.lineWrap(8);
+        await MobiiotPrinter.unbindingPrinter();
+        return null;
       }
-      await MobiiotPrinter.lineWrap(2);
-      await MobiiotPrinter.printText(r.gov, style: {"bold": true, "align": 1});
-      await MobiiotPrinter.printText(r.council,
-          style: {"bold": true, "align": 1});
-      await MobiiotPrinter.printText(line, style: {"align": 1, "font": 1});
-      await MobiiotPrinter.printText(r.phone, style: {"font": 1, "align": 1});
-      await MobiiotPrinter.printText(r.email, style: {"font": 1, "align": 1});
-      await MobiiotPrinter.printText(line, style: {"align": 1, "font": 1});
-      await MobiiotPrinter.printText(r.title,
-          style: {"bold": true, "align": 1});
-      await MobiiotPrinter.printText(r.recNumber, style: {
-        "font": 1,
-        "bold": false,
-      });
-      await MobiiotPrinter.printText(r.payer, style: {
-        "font": 1,
-        "bold": false,
-      });
-      await MobiiotPrinter.printText(r.receTotal, style: {
-        "font": 1,
-        "bold": false,
-      });
-      await MobiiotPrinter.printText(r.status, style: {
-        "font": 1,
-        "bold": false,
-      });
-      await MobiiotPrinter.lineWrap(1); // Jump 2 lines
-      // Center align
-      for (var item in items) {
-        await MobiiotPrinter.printText(
-            '${item.revenueSource.name}   ${item.quantity} x ${currency.format(item.amount)}',
-            style: {"font": 1, "align": 2});
-      }
-      await MobiiotPrinter.printText(line, style: {"align": 1, "font": 1});
-      await MobiiotPrinter.printText(r.paid, style: {"align": 2, "font": 1});
-      await MobiiotPrinter.lineWrap(2);
-      await MobiiotPrinter.printText(r.paidDate, style: {"font": 1});
-      await MobiiotPrinter.printText(r.printedBy, style: {"font": 1});
-      await MobiiotPrinter.lineWrap(8);
-      await MobiiotPrinter.unbindingPrinter();
-      return null;
+      debugPrint("not connected");
+      return 'Print not connected';
+    } catch( e) {
+      notifyError(e.toString());
+      return e.toString();
     }
-    debugPrint("not connected");
-    return 'Print not connected';
+
   }
 
   backGroundSyncTransaction(String taxCollectorUuid) async {
